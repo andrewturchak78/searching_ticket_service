b6d075c104480061fea838b1dc0bcbfdeee83232
Andrei <andrewturchak78@mail.ru>
2023-01-23 22:04:46 +0300

    comparator added

---
+++ src/main/java/org/example/TicketByPriceAscComparator.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class TicketByPriceAscComparator implements Comparator<Ticket> {
+    @Override
+    public int compare(Ticket o1, Ticket o2) {
+        if(o1.getTime() > o2.getTime()) {
+            return 1;
+        } else if(o1.getTime() < o2.getTime()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+}
